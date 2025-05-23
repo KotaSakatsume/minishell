6880793af3732c12955423485173529506d5ba77
Kouta Sakatsume <kosakats@c6r1s12.42tokyo.jp>
2025-05-23 16:44:07 +0900

    add_exit

--- built_in/builtin_exit.c
+++ built_in/builtin_exit.c
@@ -6,28 +6,94 @@
 /*   By: kosakats <kosakats@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 19:24:26 by kosakats          #+#    #+#             */
-/*   Updated: 2025/05/19 18:58:17 by kosakats         ###   ########.fr       */
+/*   Updated: 2025/05/23 14:35:34 by kosakats         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-void	handle_exit(char **av)
+#define MAX_EXIT_STATUS 255
+
+// 引数の検証
+int	validate_exit_arguments(char **args)
 {
 	int	i;
 
 	i = 0;
-	if (strcmp(av[1], "exit") == 0)
+	// 引数がない場合
+	if (args[1] == NULL)
+		return (0);
+	// 引数が数値か確認
+	while (args[1][i])
 	{
-		printf("OK\n");
-		return ;
+		if (!isdigit(args[1][i]))
+		{
+			fprintf(stderr, "exit: %s: numeric argument required\n", args[1]);
+			return (2);
+		}
+		i++;
 	}
-	return ;
+	// 引数が多すぎる場合
+	if (args[2] != NULL)
+	{
+		fprintf(stderr, "exit: too many arguments\n");
+		return (1);
+	}
+	return (0);
 }
 
-int	main(int ac, char **av, char **envp)
+// 数値引数の変換
+int	convert_exit_status(char *arg)
 {
-	if (ac > 1)
-		handle_exit(av);
+	long	status;
+
+	status = strtol(arg, NULL, 10);
+	// 負数や範囲外の値を調整
+	if (status < 0)
+		return (256 + (status % 256));
+	return (status % 256);
+}
+
+// プログラム終了
+void	execute_exit(int exit_status)
+{
+	printf("exit\n");
+	exit(exit_status);
+}
+
+// メインの exit 処理
+void	handle_exit(char **args)
+{
+	int	validation_result;
+	int	exit_status;
+
+	// 引数を検証
+	validation_result = validate_exit_arguments(args);
+	if (validation_result == 1) // 引数が多すぎる
+		return ;
+	if (validation_result == 2) // 数値でない引数
+		execute_exit(MAX_EXIT_STATUS);
+	// 引数が数値の場合、終了ステータスを計算
+	if (args[1])
+	{
+		exit_status = convert_exit_status(args[1]);
+		printf("exit_status: %d\n", exit_status);
+	}
+	else
+		exit_status = 0;
+	// プログラムを終了
+	execute_exit(exit_status);
+}
+
+// テスト用メイン関数
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+		handle_exit(argv);
 	return (0);
 }
