df193bae5549c1e77a152a183fdc632c51659e9e
reprise39 <reprise39@yahoo.co.jp>
2025-06-05 16:33:47 +0900

    test

--- SRCS/expander.c
+++ SRCS/expander.c
@@ -6,7 +6,7 @@
 /*   By: mkuida <reprise39@yahoo.co.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 23:23:04 by mkuida            #+#    #+#             */
-/*   Updated: 2025/06/01 23:28:10 by mkuida           ###   ########.fr       */
+/*   Updated: 2025/06/05 16:15:37 by mkuida           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,42 @@
 
 void	expander(t_job *job_head)
 {
-	expand_doller(job_head);
+	// expand_doller(job_head);
 	delete_quote(job_head);
 	combine_nospace_token(job_head);
+	exchange_token_to_char(job_head);
 }
 
-void	expand_doller(t_job *job_head)
+void	delete_quote(t_job *job_head)
 {
-	expand_doller_
+	t_job *job_ptr;
+	job_ptr = job_head;
+
+	while(job_ptr != NULL)
+	{
+		delete_quote_pipeline(job_ptr);
+		job_ptr = job_ptr->next;
+	}
+}
+
+void delete_doller_pipeline(t_job *job_ptr)	
+{
+	t_pipeline *pipeline_ptr;
+	pipeline_ptr = job_ptr->pipeline;
+	while(pipeline_ptr != NULL)
+	{
+		delete_quote_cmd(pipeline_ptr);
+		pipeline_ptr = pipeline_ptr->next;
+	}
+}
+
+void delete_quote_cmd(t_pipeline *pipeline_ptr)
+{
+	t_cmd *cmd_ptr;
+	cmd_ptr = pipeline_ptr->cmd;
+	while(cmd_ptr != NULL)
+	{
+		set_token_remove_quote(cmd_ptr);
+		cmd_ptr++;
+	}
 }
--- SRCS/readline.c
+++ SRCS/readline.c
@@ -6,7 +6,7 @@
 /*   By: mkuida <reprise39@yahoo.co.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 19:09:23 by mkuida            #+#    #+#             */
-/*   Updated: 2025/06/01 23:22:44 by mkuida           ###   ########.fr       */
+/*   Updated: 2025/06/05 16:31:52 by mkuida           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,13 @@ int lexer(char *input, t_shell_env *shell_env_ptr)
 	lexer_set_token_vals(split_token); //一部expanderの内容も実装してしまった
 	// marge_same_margeid_token(split_token); //これはexpanderで実装
 	print_token(split_token);
-	// 以下パーサー
+	// 以下パーサー実行部分
 	t_job *job_head;
-	
 	job_head = parse_line(split_token);
+	//　以下パーサーの結果表示部分
 	dump_jobs(job_head);
 	// jobに入らなかったtokenをfreeする必要あり
+	// 作成中のEXPANDER
 	expander(job_head);
 	// print_shell_env(shell_env_ptr);
 	// TODO: split_tokenのメモリ解放を実装
