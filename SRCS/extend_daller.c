888abe5dd1a7c1ac6ac69b560cc8f84d5e62f645
reprise39 <reprise39@yahoo.co.jp>
2025-06-28 10:25:59 +0900

    daller leak taiou

--- SRCS/extend_daller.c
+++ SRCS/extend_daller.c
@@ -6,20 +6,20 @@
 /*   By: mkuida <reprise39@yahoo.co.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 07:53:28 by mkuida            #+#    #+#             */
-/*   Updated: 2025/06/28 09:39:27 by mkuida           ###   ########.fr       */
+/*   Updated: 2025/06/28 10:25:22 by mkuida           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static char	*search_end_daller_ptr(char *ptr)
+static char *search_end_daller_ptr(char *ptr)
 {
 	while (*ptr != '\0' && *ptr != '$')
 		ptr++;
 	return (ptr);
 }
 
-static char	*serach_start_daller_ptr(char *input)
+static char *serach_start_daller_ptr(char *input)
 {
 	while (*input != '\0')
 	{
@@ -36,10 +36,10 @@ static char	*serach_start_daller_ptr(char *input)
 	return (NULL);
 }
 
-char	*serch_indaller_str(char *daller_str_ptr, char *daller_end_ptr)
+char *serch_indaller_str(char *daller_str_ptr, char *daller_end_ptr)
 {
-	char	*dest;
-	int		i;
+	char *dest;
+	int i;
 
 	i = 0;
 	daller_str_ptr++;
@@ -49,10 +49,10 @@ char	*serch_indaller_str(char *daller_str_ptr, char *daller_end_ptr)
 	return (dest);
 }
 
-char	*serch_and_expand_env(char *target, t_shell_env *t_shellenv_ptr)
+char *serch_and_expand_env(char *target, t_shell_env *t_shellenv_ptr)
 {
-	t_env	*env;
-	char	*dest;
+	t_env *env;
+	char *dest;
 
 	env = t_shellenv_ptr->env_list;
 	while ((env != NULL))
@@ -69,42 +69,42 @@ char	*serch_and_expand_env(char *target, t_shell_env *t_shellenv_ptr)
 	return (dest);
 }
 
-static void	check_and_expand_question_mode(char *daller_str_ptr,
-		t_token *token_ptr, t_shell_env *t_shellenv_ptr)
+static void check_and_expand_question_mode(char *daller_str_ptr,
+										   t_token *token_ptr, t_shell_env *t_shellenv_ptr)
 {
-	char	*daller_end_ptr;
-	char	*temp_str;
-	char	*ahead_str;
-	char	*behind_str;
+	char *daller_end_ptr;
+	char *temp_str;
+	char *ahead_str;
+	char *behind_str;
 
 	daller_end_ptr = daller_str_ptr + 2;
-	ahead_str = ft_strndup(token_ptr->value, ((daller_str_ptr)
-				- (token_ptr->value)));
+	ahead_str = ft_strndup(token_ptr->value, ((daller_str_ptr) - (token_ptr->value)));
 	behind_str = ft_strndup(daller_end_ptr, ft_strlen(daller_end_ptr));
 	temp_str = combine_str_and_free_oldstr(ahead_str,
-			ft_itoa(t_shellenv_ptr->exit_status));
+										   ft_itoa(t_shellenv_ptr->exit_status));
 	temp_str = combine_str_and_free_oldstr(temp_str, behind_str);
 	free(token_ptr->value);
 	token_ptr->value = temp_str;
 }
 
-static void	check_and_expand_normal_mode(char *daller_str_ptr,
-		t_token *token_ptr, t_shell_env *t_shellenv_ptr)
+static void check_and_expand_normal_mode(char *daller_str_ptr,
+										 t_token *token_ptr, t_shell_env *t_shellenv_ptr)
 {
-	char	*daller_end_ptr;
-	char	*indaller_str;
-	char	*expandered_str;
-	char	*temp_str;
+	char *daller_end_ptr;
+	char *indaller_str;
+	char *expandered_str;
+	char *temp_str;
 
 	daller_end_ptr = search_end_daller_ptr(daller_str_ptr + 1);
 	indaller_str = ft_strndup((daller_str_ptr + 1), (daller_end_ptr - (daller_str_ptr + 1)));
 	// printf("str = %p : end = %p\n",daller_str_ptr,daller_end_ptr);
 	// printf("indaller_str = %s\n",indaller_str);
 	expandered_str = serch_and_expand_env(indaller_str, t_shellenv_ptr);
+	free(indaller_str);
 	temp_str = combine_str_and_free_oldstr(ft_strndup(token_ptr->value,
 				((daller_str_ptr) - (token_ptr->value))), expandered_str);
 	temp_str = combine_str_and_free_oldstr(temp_str, ft_strndup(daller_end_ptr,
-				ft_strlen(daller_end_ptr)));
+																ft_strlen(daller_end_ptr)));
 	free(token_ptr->value);
 	token_ptr->value = temp_str;
 }
@@ -112,29 +112,28 @@ static void	check_and_expand_normal_mode(char *daller_str_ptr,
 static void	check_and_expand_daller_mode(char *daller_str_ptr,
 		t_token *token_ptr)
 {
-	char	*daller_end_ptr;
-	char	*temp_str;
-	char	*ahead_str;
-	char	*behind_str;
+	char *daller_end_ptr;
+	char *temp_str;
+	char *ahead_str;
+	char *behind_str;
 
 	daller_end_ptr = daller_str_ptr + 2;
-	ahead_str = ft_strndup(token_ptr->value, ((daller_str_ptr)
-				- (token_ptr->value)));
+	ahead_str = ft_strndup(token_ptr->value, ((daller_str_ptr) - (token_ptr->value)));
 	behind_str = ft_strndup(daller_end_ptr, ft_strlen(daller_end_ptr));
 	temp_str = combine_str_and_free_oldstr(ahead_str, behind_str);
 	free(token_ptr->value);
 	token_ptr->value = temp_str;
 }
 
-static void	check_and_expand(t_token *token_ptr, t_shell_env *t_shellenv_ptr)
+static void check_and_expand(t_token *token_ptr, t_shell_env *t_shellenv_ptr)
 {
-	char	*daller_str_ptr;
+	char *daller_str_ptr;
 
 	while (serach_start_daller_ptr(token_ptr->value) != NULL)
 	{
 		daller_str_ptr = serach_start_daller_ptr(token_ptr->value);
 		if (*(daller_str_ptr + 1) == '\0')
-			return ;
+			return;
 		if (*(daller_str_ptr + 1) == '$')
 		{
 			check_and_expand_daller_mode(daller_str_ptr, token_ptr);
@@ -142,31 +141,30 @@ static void	check_and_expand(t_token *token_ptr, t_shell_env *t_shellenv_ptr)
 		else if (*(daller_str_ptr + 1) == '?')
 		{
 			check_and_expand_question_mode(daller_str_ptr, token_ptr,
-				t_shellenv_ptr);
+										   t_shellenv_ptr);
 		}
 		else
 		{
 			check_and_expand_normal_mode(daller_str_ptr, token_ptr,
-				t_shellenv_ptr);
+										 t_shellenv_ptr);
 		}
 	}
 }
 
-void	extend_daller(t_pipeline *pipeline_ptr, t_shell_env *t_shellenv_ptr)
+void extend_daller(t_pipeline *pipeline_ptr, t_shell_env *t_shellenv_ptr)
 {
-	int		i;
-	t_cmd	*cmd_ptr;
-	t_token	*token_ptr;
+	int i;
+	t_cmd *cmd_ptr;
+	t_token *token_ptr;
 
 	i = 0;
 	cmd_ptr = pipeline_ptr->cmd;
-	if(cmd_ptr->token == NULL) // red taiou
+	if (cmd_ptr->token == NULL) // red taiou
 		return;
 	token_ptr = cmd_ptr->token[i];
 	while (token_ptr != NULL)
 	{
-		if (token_ptr->status->in_quote == QUOTE_DOUBLE
-			|| token_ptr->status->in_quote == QUOTE_OUT)
+		if (token_ptr->status->in_quote == QUOTE_DOUBLE || token_ptr->status->in_quote == QUOTE_OUT)
 		{
 			check_and_expand(token_ptr, t_shellenv_ptr);
 		}
