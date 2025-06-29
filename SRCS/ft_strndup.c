9f75795d1dd28c8d660c68e07abc231df63fac5b
Kouta Sakatsume <kosakats@c6r1s8.42tokyo.jp>
2025-06-29 11:40:25 +0900

    strndup

--- SRCS/ft_strndup.c
+++ SRCS/ft_strndup.c
@@ -6,26 +6,25 @@
 /*   By: kosakats <kosakats@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 11:21:46 by kosakats          #+#    #+#             */
-/*   Updated: 2025/05/17 16:07:11 by kosakats         ###   ########.fr       */
+/*   Updated: 2025/06/29 11:40:02 by kosakats         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <string.h>
+#include "minishell.h"
 
 char	*ft_strndup(const char *s, size_t n)
 {
-	size_t len = strnlen(s, n);  // 実際にコピーする長さを計算
-	char *dup = malloc(len + 1); // 終端文字分を含む領域を確保
+	size_t	len;
+	char	*dup;
+
+	len = strnlen(s, n);
+	dup = malloc(len + 1);
 	if (!dup)
-	{
-		return (NULL); // メモリ確保失敗時
-	}
-	memcpy(dup, s, len); // 必要な文字をコピー
-	dup[len] = '\0';     // ヌル文字で終端
+		return (NULL);
+	memcpy(dup, s, len);
+	dup[len] = '\0';
 	return (dup);
 }
-#include <stdio.h>
 
 // int	main(void)
 // {
