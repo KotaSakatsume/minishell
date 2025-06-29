80257a6744a70f416735a2e1954ee4044e8a738a
reprise39 <reprise39@yahoo.co.jp>
2025-06-29 09:07:58 +0900

    norm : keibi  : komakainaiyounomi

--- SRCS/utils_free_struct.c
+++ SRCS/utils_free_struct.c
@@ -6,16 +6,16 @@
 /*   By: mkuida <reprise39@yahoo.co.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 18:51:27 by mkuida            #+#    #+#             */
-/*   Updated: 2025/06/28 16:10:47 by mkuida           ###   ########.fr       */
+/*   Updated: 2025/06/29 09:05:08 by mkuida           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void free_all_token(t_token **dest)
+void	free_all_token(t_token **dest)
 {
-	t_token *now_token;
-	t_token *next_token;
+	t_token	*now_token;
+	t_token	*next_token;
 
 	now_token = *dest;
 	if (now_token != NULL)
@@ -27,7 +27,7 @@ void free_all_token(t_token **dest)
 	free(dest);
 }
 
-void free_token(t_token *free_dest)
+void	free_token(t_token *free_dest)
 {
 	if (free_dest->value != NULL)
 		free(free_dest->value);
@@ -36,29 +36,32 @@ void free_token(t_token *free_dest)
 	free(free_dest);
 }
 
-static bool token_stat_type_is_conjunc(t_token_type t_type)
+static bool	token_stat_type_is_conjunc(t_token_type t_type)
 {
-	if (t_type == TYPE_PIPE || t_type == TYPE_REDIRECT_IN || t_type == TYPE_REDIRECT_OUT || t_type == TYPE_REDIRECT_APPEND || t_type == TYPE_REDIRECT_HEREDOC || t_type == TYPE_SEMICOLON)
+	if (t_type == TYPE_PIPE || t_type == TYPE_REDIRECT_IN
+		|| t_type == TYPE_REDIRECT_OUT || t_type == TYPE_REDIRECT_APPEND
+		|| t_type == TYPE_REDIRECT_HEREDOC || t_type == TYPE_SEMICOLON)
 		return (true);
 	else
 		return (false);
 }
 
-static bool token_stat_type_is_red(t_token_type t_type)
+static bool	token_stat_type_is_red(t_token_type t_type)
 {
-	if (t_type == TYPE_REDIRECT_IN || t_type == TYPE_REDIRECT_OUT || t_type == TYPE_REDIRECT_APPEND || t_type == TYPE_REDIRECT_HEREDOC)
+	if (t_type == TYPE_REDIRECT_IN || t_type == TYPE_REDIRECT_OUT
+		|| t_type == TYPE_REDIRECT_APPEND || t_type == TYPE_REDIRECT_HEREDOC)
 		return (true);
 	else
 		return (false);
 }
 
-void free_conjunc_token(t_token **dest)
+void	free_conjunc_token(t_token **dest)
 {
-	t_token *now_token;
-	t_token *next_token;
-	bool flag_red;
-	bool flag_after_red;
-	
+	t_token	*now_token;
+	t_token	*next_token;
+	bool	flag_red;
+	bool	flag_after_red;
+
 	next_token = *dest;
 	flag_after_red = false;
 	while (next_token != NULL)
@@ -68,7 +71,8 @@ void free_conjunc_token(t_token **dest)
 		// printf("now_token_type : %d\n",now_token->status->token_type);
 		next_token = now_token->next;
 		// printf("next_token = %p\n",next_token);
-		if (token_stat_type_is_conjunc(now_token->status->token_type) == true || flag_after_red == true)
+		if (token_stat_type_is_conjunc(now_token->status->token_type) == true
+			|| flag_after_red == true)
 		{
 			// printf("delete = %s\n", now_token->value);
 			// fflush(stdout);
