d1eaf4d69c0d6f6eb481f3fb0a9de3e233e864fa
Kouta Sakatsume <kosakats@c6r1s8.42tokyo.jp>
2025-06-29 11:12:19 +0900

    ft_handle

--- SRCS/ft_handle_external.c
+++ SRCS/ft_handle_external.c
@@ -6,7 +6,7 @@
 /*   By: kosakats <kosakats@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 23:23:04 by mkuida            #+#    #+#             */
-/*   Updated: 2025/06/20 14:28:30 by kosakats         ###   ########.fr       */
+/*   Updated: 2025/06/29 11:11:39 by kosakats         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,49 +42,50 @@ void	fork_and_execute(t_pipeline *pipeline, t_shell_env *shell_env,
 	exit(EXIT_FAILURE); // 子プロセス終了
 }
 
-void	parent_process_cleanup(t_pipeline *pipeline, int prev_pipe[2],
-		int pipe_fd[2], pid_t pid, t_shell_env *shell_env)
+void	handle_exit_status(t_shell_env *shell_env, int status)
 {
-	int	status;
-
-	if (prev_pipe[0] != -1)
-		close(prev_pipe[0]);
-	if (pipeline->next)
-	{
-		close(pipe_fd[1]); // 親プロセスでは書き込み側は不要
-		prev_pipe[0] = pipe_fd[0];
-	}
-	// 子プロセスの終了ステータスを取得
-	if (waitpid(pid, &status, 0) == -1)
-	{
-		perror("waitpid");
-		update_exit_status(shell_env, 1); // 子プロセス管理失敗
-		return ;
-	}
-	// 終了ステータスを更新
 	if (WIFEXITED(status))
 		update_exit_status(shell_env, WEXITSTATUS(status));
 	else if (WIFSIGNALED(status))
 		update_exit_status(shell_env, 128 + WTERMSIG(status));
 }
 
+void	parent_process_cleanup(t_pipeline *pipeline, int prev_pipe[2],
+		int pipe_fd[2])
+{
+	if (prev_pipe[0] != -1)
+		close(prev_pipe[0]);
+	if (pipeline->next)
+	{
+		close(pipe_fd[1]);
+		prev_pipe[0] = pipe_fd[0];
+	}
+}
+
 void	handle_external(t_pipeline *pipeline, t_shell_env *shell_env,
 		int prev_pipe[2], int pipe_fd[2])
 {
 	pid_t	pid;
+	int		status;
 
 	setup_pipe(pipeline, pipe_fd, shell_env);
 	if (pipeline->next && pipe_fd[0] < 0)
-		return ; // パイプ作成失敗時は終了
+		return ;
 	pid = fork();
 	if (pid < 0)
 	{
 		perror("fork");
-		update_exit_status(shell_env, 1); // フォーク失敗
+		update_exit_status(shell_env, 1);
 		return ;
 	}
 	if (pid == 0)
 		fork_and_execute(pipeline, shell_env, prev_pipe, pipe_fd);
 	else
-		parent_process_cleanup(pipeline, prev_pipe, pipe_fd, pid, shell_env);
+	{
+		parent_process_cleanup(pipeline, prev_pipe, pipe_fd);
+		if (waitpid(pid, &status, 0) != -1)
+			handle_exit_status(shell_env, status);
+		else
+			perror("waitpid");
+	}
 }
